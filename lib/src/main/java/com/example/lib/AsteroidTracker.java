2aca4646df15b8ae7d442552dcde7159fc4b19a1
karnappatel <karnapp2@illinois.edu>
2019-04-24 11:57:40 -0500

    asteroidtracker

--- lib/src/main/java/com/example/lib/AsteroidTracker.java
+++ lib/src/main/java/com/example/lib/AsteroidTracker.java
@@ -1,14 +1,17 @@
 package com.example.lib;
-import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import jdk.nashorn.internal.parser.JSONParser;
+
 public class AsteroidTracker {
 
+    private static JSONParser newParser = new JSONParser();
+
     public String getAsteroidList() {
+
 
     }
 

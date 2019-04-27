d9281a8cc20556524764f09ce049fbc4d1e58b61
karnappatel <karnapp2@illinois.edu>
2019-04-27 18:48:44 -0500

    asteroidtracker

--- app/src/main/java/com/example/spaceapp/MainActivity.java
+++ app/src/main/java/com/example/spaceapp/MainActivity.java
@@ -22,6 +22,7 @@ import android.widget.VideoView;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -53,13 +54,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(JSONObject response) {
 
-            },
-            new Response.ErrorListener() {
-
-
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
             }
-        });
+    });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);

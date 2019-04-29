59319f0e91191591d2c66bec7d2f50a8560d17b8
Aditya Gupta <adityag5@illinois.edu>
2019-04-29 16:26:21 -0500

    1

--- app/src/main/java/com/example/spaceapp/ListActivity.java
+++ app/src/main/java/com/example/spaceapp/ListActivity.java
@@ -1,5 +1,20 @@
 package com.example.spaceapp;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+
 
 public class ListActivity {
+    Button button;
+    public class App2Activity extends Activity {
 
+        Button button;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.list_page);
+        }
+    }
 }

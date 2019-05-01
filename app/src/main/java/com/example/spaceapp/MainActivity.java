52f7847576ea30a9a291d244d45311b6148f35b5
karnappatel <karnapp2@illinois.edu>
2019-05-01 04:27:14 -0500

    asteroidtracker

--- app/src/main/java/com/example/spaceapp/MainActivity.java
+++ app/src/main/java/com/example/spaceapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.spaceapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;

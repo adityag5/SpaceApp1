2759e19ad0a8272c606f20b6ba99c5f2fca77b39
Aditya Gupta <adityag5@illinois.edu>
2019-05-01 02:32:38 -0500

    1

--- app/src/main/java/com/example/spaceapp/ListActivity.java
+++ app/src/main/java/com/example/spaceapp/ListActivity.java
@@ -56,8 +56,8 @@ public class ListActivity extends AppCompatActivity {
 
         final SimpleAdapter sa = new SimpleAdapter(this, list,
                 R.layout.individual,
-                new String[] { "line1", "line2", "line3", "line4"},
-                new int[] {R.id.line_a, R.id.line_b, R.id.line_c, R.id.line_d} );
+                new String[] { "line1", "line2", "line3", "line4", "line5", "line6", "line7", "line8"},
+                new int[] {R.id.line_a, R.id.line_b, R.id.line_c, R.id.line_d, R.id.line_e, R.id.line_f, R.id.line_g, R.id.line_g} );
 
 
         requestQueue = Volley.newRequestQueue(this);
@@ -77,7 +77,10 @@ public class ListActivity extends AppCompatActivity {
                         asteroidMultiArray[i][1] = AsteroidTracker.getAsteroidUrl(asteroid);
                         asteroidMultiArray[i][2] = AsteroidTracker.getCloseApproachDate(asteroid);
                         asteroidMultiArray[i][3] = AsteroidTracker.getOrbitBody(asteroid);
-
+                        asteroidMultiArray[i][4] = AsteroidTracker.getAsteroidSizeMetersMin(asteroid);
+                        asteroidMultiArray[i][5] = AsteroidTracker.getAsteroidSizeMetersMax(asteroid);
+                        asteroidMultiArray[i][6] = AsteroidTracker.getIsHazardous(asteroid);
+                        asteroidMultiArray[i][7] = AsteroidTracker.getOrbitalPeriod(asteroid);
 
                     }
                     HashMap<String, String> item = new HashMap<>();
@@ -93,6 +96,10 @@ public class ListActivity extends AppCompatActivity {
                         resultsMap.put("line2", "JPL URL : " + asteroidMultiArray[i][1]);
                         resultsMap.put("line3", "Close Approach Date : " + asteroidMultiArray[i][2]);
                         resultsMap.put("line4", "Orbit Body : " + asteroidMultiArray[i][3]);
+                        resultsMap.put("line5", "Minimum Asteroid Size (meters) : " + asteroidMultiArray[i][4]);
+                        resultsMap.put("line6", "Maximum Asteroid Size (meters) : " + asteroidMultiArray[i][5]);
+                        resultsMap.put("line7", "Is it Hazardous? : " + asteroidMultiArray[i][6]);
+                        resultsMap.put("line8", "Orbital Period (days) : " + asteroidMultiArray[i][7]);
                         list.add(resultsMap);
                     }
                     results.setAdapter(sa);
